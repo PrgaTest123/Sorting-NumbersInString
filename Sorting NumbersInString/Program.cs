cbc6440dcf12721377fba462e27aec6de6d4f1bb
PrgaTest123 <marintest0@gmail.com>
2022-09-29 21:25:39 +0200

    Add project files.

---
+++ Sorting NumbersInString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+namespace Sorting_NumbersInString
+{
+    public class SortingNumbersInString
+    {
+        public static string Order(string words)
+        {
+            var wordsOrder = words.Split(' ').OrderBy(qq=>new String(qq.Where(ch=>char.IsDigit(ch)).Select(qq=>qq).ToArray())).ToArray();
+            var theString = String.Join(" ", wordsOrder);
+            return theString;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = "is2 Thi1s T4est 3a";
+            Console.WriteLine(SortingNumbersInString.Order(test));
+        }
+    }
+}
